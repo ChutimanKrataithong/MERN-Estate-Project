254a798a0af805bf241da292b1949db40202080b
Chutiman <chutiman.krataithong@gmail.com>
2023-12-25 23:07:02 +0700

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
@@ -18,3 +20,5 @@ export default function App() {
     </BrowserRouter>
   )
 }
+
+28.13
